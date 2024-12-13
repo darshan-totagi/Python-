cb234d2b02ee306219d10b5e1c876134d597664c
darshan-totagi <darshantotagi7975@gmail.com>
2024-12-13 21:20:09 +0530

    Add files via upload

---
+++ input_casting.py
@@ -0,0 +1,8 @@
+# a=int(input("enter the number:"))#enter the number:30
+# print("your value is:",a)#your value is: 30
+
+a=eval(input("enter the number:"))#enter the number:30
+b=eval(input("enter the number:"))
+print("your value is:",a+b)
+
+#eval will take values in int float and bytes
