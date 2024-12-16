bfa2cb345237524e78c68a77f867d576a33c67bf
darshan totagi <darshantotagi7975@gmail.com>
2024-12-16 18:54:56 +0530

    Add files via upload

---
+++ string_format.py
@@ -0,0 +1,11 @@
+a="welcome to"
+
+a="welcome {0} to {1}".format("darshan","davangere")
+print(a)
+
+a="welcome {a:^10} to {b}".format(a=30,b=90)
+print(a)
+a="welcome {a:<10} to {b}".format(a=30,b=90)
+print(a)
+a="welcome {a:>10} to {b}".format(a=30,b=90)
+print(a)
